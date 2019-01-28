90664c914c36f0eafad73baae716f756533bfd0c
Rasmus Kjær Dam <Rallethekid@gmail.com>
2019-01-28 08:50:38 +0100

    opgaven lagt op

---
+++ ADT/GenericMyLinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    public class MyLinkedList<T>
+    {
+        // Insert code from MyLinkedList here ... 
+    }
+}
