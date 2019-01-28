4001bd00e96be950964ddee3154cc78f6b167d91
Kristian Kastrup Pedersen <kris9139@edu.eal.dk>
2019-01-28 08:44:31 +0100

    sdcfgh

---
+++ .vs/ex32-linkedlist/ClubMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex32_linkedlist
+{
+    public class ClubMember
+    {
+        public int nr;
+        public string FNavn;
+        public string LNavn;
+        public int alder;
+
+        public ClubMember(int Nr, string fname, string ename, int Alder)
+        {
+            this.nr = Nr;
+            this.FNavn = fname;
+            this.LNavn = ename;
+            this.alder = Alder;
+
+        }
+        public override string ToString()
+        {
+            return "" + nr + " " + FNavn + " " + LNavn + " " + alder; 
+        }
+    }
+}
---
+++ .vs/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ee7889e-4eda-49b3-8f5a-ed609607e828")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
